26026ad6cbd76b763affd9de74151efe521ae317
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2021-11-03 12:01:06 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/Convertidor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class Convertidor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return "5";
+            }
+            else
+            {
+                return "0";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
